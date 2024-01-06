ab04001d9c0f4745bdfaba111725b346b7a48670
mohammad kouchenani <mammadkouchenani@gmail.com>
2024-01-06 03:33:04 +0330

    Route and LoginRegisterController are done

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(\App\Http\Controllers\Auth\LoginRegisterController::class)->group(function (){
+    Route::get('/register', 'register')->name('register');
+    Route::post('/register', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', 'logout')->name('logout');
+});
